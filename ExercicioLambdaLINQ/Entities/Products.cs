bcf39d204d9a2c11e5bf8cd1768c6ed91bc7f3fc
Thiago Menezes <thiagocwbpr@gmail.com>
2022-04-13 10:48:53 -0300

    Sistem que solicita o caminho do arquivo ao usuário, para realizar a leitura dos dados internos sobre produtos e preço, o sistema separa os dados do arquivo, adiciona em uma lista, realiza o calculo de média de valor, e retorna ao usuário quais produtos estão abaixo da média de preço da tabela de arquivos. É um exercicio de consulta de tabelas, que pode ser utilizado em banco de dados SQL juntamente a um sistema de consultas de produtos.

---
+++ ExercicioLambdaLINQ/Entities/Products.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace ExercicioLambdaLINQ.Entities {
+    internal class Products {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Products(string Name, double Price) {
+            this.Name = Name;
+            this.Price = Price;
+        }
+        public override string ToString() {
+            return Name + " " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- ExercicioLambdaLINQ/Program.cs
+++ ExercicioLambdaLINQ/Program.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.IO;
+using ExercicioLambdaLINQ.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
 
 namespace ExercicioLambdaLINQ {
     internal class Program {
         static void Main(string[] args) {
-            
 
+            Console.Write("Enter full file Path: ");
+            string path = Console.ReadLine();
+            // string path = @"C:\Apps C#\ExercicioLambdaLINQ\Products.txt";
+
+            List<Products> ProdList = new List<Products>();
+
+            using (StreamReader sr = File.OpenText(path)) {
+                while (!sr.EndOfStream) {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+                    ProdList.Add(new Products(name, price));
+                }
+            }
+            var avg = ProdList.Select(p => p.Price).Average();
+            Console.WriteLine("Average Price: R$ " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var ProductName = ProdList.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+            foreach (var x in ProductName) {
+                Console.WriteLine(x);
+            }
 
         }
     }
